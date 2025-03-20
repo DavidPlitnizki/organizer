5aaec1e62165f63835801a18d21243c07d3b5ec7
DavidPlitnizki <dplitnizki@gmail.com>
2025-03-20 23:39:13 +0200

    in progress

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -15,44 +15,69 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 import PieChart from "react-native-pie-chart";
+import StatisticsPie from "~/widgets/StatisticsPie";
+import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
+import { Label } from "~/components/ui/label";
 
-const widthAndHeight = 120;
-
-const series = [
-  { value: 430, color: "#fbd203", label: { text: "A", fontWeight: "bold" } },
-  {
-    value: 321,
-    color: "#ffb300",
-    label: { text: "mobile", fontSize: 8 },
-  },
-  {
-    value: 185,
-    color: "#ff9100",
-    label: { text: "%22", fontSize: 8 },
-  },
-];
+function RadioGroupItemWithLabel({
+  value,
+  onLabelPress,
+}: {
+  value: string;
+  onLabelPress: () => void;
+}) {
+  return (
+    <View className={"flex-row gap-2 items-center"}>
+      <RadioGroupItem aria-labelledby={`label-for-${value}`} value={value} />
+      <Label nativeID={`label-for-${value}`} onPress={onLabelPress}>
+        {value}
+      </Label>
+    </View>
+  );
+}
 
 export default function Home() {
   const [modalVisible, setModalVisible] = useState(false);
   const { colors } = useTheme();
+
+  const [value, setValue] = React.useState("All");
+
+  function onLabelPress(label: string) {
+    return () => {
+      setValue(label);
+    };
+  }
   return (
     <SafeAreaView className="relative px-2 h-full pb-20">
+      <StatisticsPie />
+
       <Card
         className={`flex flex-row w-full my-2 px-4 items-center justify-between`}
       >
-        <CardHeader className="flex flex-col px-1 max-w-72">
-          <CardTitle className={`text-xl text-text font-rubik-bold `}>
-            Statistics
+        <CardHeader className="flex flex-col px-1 max-w-72 h-48">
+          <CardTitle className={`text-2xl font-rubik-bold `}>
+            Task Filters:
           </CardTitle>
-          <CardDescription>
-            <View className="flex-1 items-center">
-              <PieChart
-                widthAndHeight={widthAndHeight}
-                series={series}
-                cover={0.5}
+          <View className="flex-1 justify-center items-start pl-2">
+            <RadioGroup
+              value={value}
+              onValueChange={setValue}
+              className="gap-3"
+            >
+              <RadioGroupItemWithLabel
+                value="All"
+                onLabelPress={onLabelPress("All")}
               />
-            </View>
-          </CardDescription>
+              <RadioGroupItemWithLabel
+                value="Todo"
+                onLabelPress={onLabelPress("Todo")}
+              />
+              <RadioGroupItemWithLabel
+                value="Done"
+                onLabelPress={onLabelPress("Done")}
+              />
+            </RadioGroup>
+          </View>
         </CardHeader>
 
         <CardFooter className="p-0 items-center justify-center gap-8">
---
+++ widgets/StatisticsPie.tsx
@@ -0,0 +1,56 @@
+import { View, Text } from "react-native";
+import React from "react";
+import PieChart from "react-native-pie-chart";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+
+const widthAndHeight = 140;
+
+const series = [
+  {
+    value: 4,
+    color: "#16a34a",
+    label: { text: "4", fontSize: 14, fontWeight: "bold" },
+  },
+  {
+    value: 6,
+    color: "#60a5fa",
+    label: { text: "6", fontSize: 14, fontWeight: "bold" },
+  },
+];
+
+const StatisticsPie = () => {
+  return (
+    <Card
+      className={`flex flex-row w-full my-2 px-4 items-center justify-between`}
+    >
+      <CardHeader className="flex flex-col px-1 max-w-72 h-48">
+        <CardTitle className={`text-2xl font-rubik-bold `}>
+          Tasks Statistics:
+        </CardTitle>
+        <View className="mt-6">
+          <Text className="font-rubik-semibold text-lg pl-2">
+            All Tasks - 10
+          </Text>
+          <Text className="font-rubik-semibold text-lg pl-2">
+            Todo - 6 <View className="w-3 h-3 bg-blue-400" />
+          </Text>
+          <Text className="font-rubik-semibold text-lg pl-2">
+            Done - 4 <View className="w-3 h-3 bg-green-600" />
+          </Text>
+        </View>
+      </CardHeader>
+
+      <CardFooter className="p-0 items-center justify-center gap-8">
+        <PieChart widthAndHeight={widthAndHeight} series={series} cover={0.5} />
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default StatisticsPie;
