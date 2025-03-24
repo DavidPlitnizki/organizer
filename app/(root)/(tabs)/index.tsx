7cab7ddd4c8f4dc78f33b52ffe4b70bb296ab480
DavidPlitnizki <dplitnizki@gmail.com>
2025-03-24 14:54:20 +0200

    updadted

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,33 +1,14 @@
-import { FlatList, View } from "react-native";
-import React, { useMemo, useState } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { DATA } from "~/mockData";
-import ModalTask from "~/widgets/ModalTask";
-import CardTask from "~/widgets/CardTask";
-import { RadioGroupItem } from "~/components/ui/radio-group";
-import { Label } from "~/components/ui/label";
-import SearchCard from "~/widgets/SearchCard";
-
-function RadioGroupItemWithLabel({
-  value,
-  onLabelPress,
-}: {
-  value: string;
-  onLabelPress: () => void;
-}) {
-  return (
-    <View className={"flex-row gap-2 items-center"}>
-      <RadioGroupItem aria-labelledby={`label-for-${value}`} value={value} />
-      <Label nativeID={`label-for-${value}`} onPress={onLabelPress}>
-        {value}
-      </Label>
-    </View>
-  );
-}
+import { FlatList } from 'react-native';
+import React, { useMemo, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { DATA } from '~/mockData';
+import ModalTask from '~/widgets/ModalTask';
+import CardTask from '~/widgets/CardTask';
+import SearchCard from '~/widgets/SearchCard';
 
 export default function Home() {
   const [modalVisible, setModalVisible] = useState(false);
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState('');
 
   const filteredData = useMemo(() => {
     return DATA.filter((dInfo) =>
